139700c8109c026e412446d4388635b2f92af6a8
Martin C. Doege <mdoege@compuserve.com>
2020-11-10 17:49:54 +0100

    add randomize; use nimpretty

--- tix.nim
+++ tix.nim
@@ -9,7 +9,10 @@ var
   render: RendererPtr
 
 window = createWindow("tix", 100, 100, 1200, 300, SDL_WINDOW_SHOWN)
-render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+render = createRenderer(window, -1, Renderer_Accelerated or
+    Renderer_PresentVsync or Renderer_TargetTexture)
+
+randomize()
 
 var
   evt = sdl2.defaultEvent
@@ -25,33 +28,33 @@ while runGame:
     if evt.kind == QuitEvent:
       runGame = false
       break
-  render.setDrawColor 0,0,0,255
+  render.setDrawColor 0, 0, 0, 255
   render.clear
   r.w = 80
   r.h = 80
 
-  render.setDrawColor 255,0,0,255
+  render.setDrawColor 255, 0, 0, 255
   shuffle(a3)
   for i in 0..int(d[0])-49:
     r.x = 10
     r.y = 10 + 100 * cast[cint](a3[i])
     render.fillRect(r)
 
-  render.setDrawColor 0,255,0,255
+  render.setDrawColor 0, 255, 0, 255
   shuffle(a9)
   for i in 0..int(d[1])-49:
     r.x = 210 + 100 * cast[cint](a9[i] mod 3)
     r.y = 10 + 100 * cast[cint](a9[i] div 3)
     render.fillRect(r)
 
-  render.setDrawColor 0,0,255,255
+  render.setDrawColor 0, 0, 255, 255
   shuffle(a6)
   for i in 0..int(d[2])-49:
     r.x = 610 + 100 * cast[cint](a6[i] mod 2)
     r.y = 10 + 100 * cast[cint](a6[i] div 2)
     render.fillRect(r)
 
-  render.setDrawColor 255,0,0,255
+  render.setDrawColor 255, 0, 0, 255
   shuffle(a9)
   for i in 0..int(d[3])-49:
     r.x = 910 + 100 * cast[cint](a9[i] mod 3)
